3a36c6913f958996049db4a42d1437c5a609711f
Printer <sriapirath1.1@gmail.com>
2025-05-27 11:17:27 +0700

    Add Const title/currentYear/isShow

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,7 +1,20 @@
 export default function AppWelcome(){
+    const title = 'Welcome to COSCI';
+    const currentYear = <p>2025</p>;
+    const isShow = true;
+
     return (
         <>
-            <p>Welcome to COSCI</p>
+            <p>{title.toUpperCase()}</p>
+            {currentYear}
+            {
+                isShow && <p>Date: 10/10/1998</p> 
+                // (If True) ถ้า isShow เป็น True จะแสดง Date 10/10/1998
+            }
+            {
+                isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>
+                // (If/Else) ถ้า isShow เป็น True แสดง Hello Next.js เป็น False แสดง Hello JavaScript
+            }
         </>
     );
 }
