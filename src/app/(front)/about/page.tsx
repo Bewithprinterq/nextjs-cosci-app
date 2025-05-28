5f702dd5172aab87a496c529a91ac3c8e116cb19
Printer <sriapirath1.1@gmail.com>
2025-05-28 14:20:21 +0700

    add api show version

--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,9 +1,14 @@
 import Contact01Page from "@/components/contact-01/contact-01";
 
-export default function About() {
+export default async function About() {
+    const data = await fetch('http://api.codingthailand.com/api/version',{next: {revalidate: 3600}});
+    // cache 1 ชม. ใส่ revalidate
+    const apiInfo = await data.json();
     return (
         <>
-            <Contact01Page/>
+        {
+            apiInfo && <Contact01Page version={apiInfo.data.version}/>
+        }
         </>
     );
 }
